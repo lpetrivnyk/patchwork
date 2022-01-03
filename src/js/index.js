d3f0c2c9baa9e21041cafadf990a2f87e806f010
Liubomyr Petrivnyk <lubomyr.petrivnyk@gmail.com>
2022-01-03 12:12:09 +0200

    build

--- src/js/index.js
+++ src/js/index.js
@@ -26,11 +26,24 @@ $(document).on('click', '.burger', function (event) {
   }
 });
 
-$(document).on('click', '.mobile-menu li.menu-item-has-children', function (event) {
+$(document).on('click', '.mobile-menu li.menu-item-has-children>a', function (event) {
   event.preventDefault();
   $('.mobile-menu li.menu-item-has-children .sub-menu').slideUp();
-  $(this).find('.sub-menu').slideDown();
-})
+  $(this).closest('li.menu-item-has-children').find('.sub-menu').slideDown();
+});
+
+$(document).on('click', '.btn-more', function (event) {
+  event.preventDefault();
+  $(this).closest('.wp-text').find('.more-txt').slideDown();
+  $(this).hide();
+  $(this).closest('.wp-text').find('.btn-less').show();
+});
+$(document).on('click', '.btn-less', function (event) {
+  event.preventDefault();
+  $(this).closest('.wp-text').find('.more-txt').slideUp();
+  $(this).hide();
+  $(this).closest('.wp-text').find('.btn-more').show();
+});
 
 
 //Menu
@@ -72,4 +85,65 @@ AOS.init({
   once: true, // whether animation should happen only once - while scrolling down
   // mirror: true, // whether elements should animate out while scrolling past them
   // anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+
+$('[data-fancybox]').fancybox({
+  // Options will go here
+  buttons : [
+    'close'
+  ],
+  wheel : false,
+  transitionEffect: "slide",
+  // thumbs          : false,
+  // hash            : false,
+  loop            : true,
+  // keyboard        : true,
+  toolbar         : false,
+  // animationEffect : false,
+  // arrows          : true,
+  clickContent    : false
+});
+
+var gallerySwiper = new Swiper(".gallerySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".page-gallery .swiper-button-next",
+    prevEl: ".page-gallery .swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+
+var reviewsSwiper = new Swiper(".reviewsSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 40,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".page-reviews .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".page-reviews .swiper-button-next",
+    prevEl: ".page-reviews .swiper-button-prev",
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
 });
